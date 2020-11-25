a53812e66c06420a31dcd819e96b565763628f7b
Sami Imran <samiimran2016@gmail.com>
2020-11-25 11:37:34 +0200

    Kick of working

---
+++ SmallProject/SmallProject/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SmallProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello World! ");
+            Console.WriteLine("I am using C# for the very first time");
+            var x = 9;
+            if (x == 9)
+                Console.WriteLine(" Its " + x + " !! ");
+            else
+                Console.WriteLine("Its " + x + " !! ");
+
+            /***
+             * To be worked on later:
+             *  How to take input 
+             ***/
+
+
+        }
+    }
+}
